2042f8222305e75d65042f0634b07109c7f207df
Juan De Dios Fernandez Camilo <juan_fernandez3@ucne.edu.do>
2020-03-15 23:56:25 -0400

    Agregar archivos de proyecto.

---
+++ Dal/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroDetalleOrden.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\SegundoParcialDb");
+        }
+    }
+}
---
+++ UI/Formulario1A.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SegundoParcial.UI
+{
+    /// <summary>
+    /// Lógica de interacción para Formulario1A.xaml
+    /// </summary>
+    public partial class Formulario1A : Window
+    {
+        public Formulario1A()
+        {
+            InitializeComponent();
+        }
+    }
+}
